a0281360efd1dd0b88975ec5e0f12453860b8405
Abourass <abourassa@assetval.com>
2018-09-04 03:50:31 -0600

    V1.0 Done

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+// Load Routes
+const index = require('./routes/index');
+
+const app = express();
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Handlebars middleware
+app.engine('.handlebars', exphbs({
+  helpers: {
+  },
+  defaultLayout: 'main',
+  partialsDir: ['./views/request/partials/', './views/partials/'],
+  extname: '.handlebars',
+}));
+app.set('view engine', 'handlebars');
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use Routes
+app.use('/', index);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
+});
